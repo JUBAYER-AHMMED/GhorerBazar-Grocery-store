03fd004b1774a553e903a32e78a981ea0ff9c0c4
JUBAYER AHMMED <jubayerahmmed105@gmail.com>
2026-03-25 12:22:13 +0600

    changed the domain in users/email.py

--- users/email.py
+++ users/email.py
@@ -3,6 +3,6 @@ from djoser import email
 class ActivationEmail(email.ActivationEmail):
     def get_context_data(self):
         context = super().get_context_data()
-        context["domain"] = "ghorer-bazar-client.vercel.app"
+        context["domain"] = "groc-ashy.vercel.app"
         context["protocol"] = "https"
         return context
